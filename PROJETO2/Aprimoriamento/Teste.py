707a24f46c8ded0720ce6acc85982db0f3e188fc
Gaybrou <vtga559@gmail.com>
2021-06-24 20:45:41 -0300

    Atualizaçao

---
+++ PROJETO2/Aprimoriamento/Teste.py
@@ -0,0 +1,11 @@
+y = 10
+x = 20
+print(f'valor de y é {y}')
+print(f'valor de x é {x}')
+#print("=" 10*)
+def soma (x, y)
+  Z = y + x 
+  return Z # O return volta o valor da soma 
+
+valor soma() # variavel 'valor' recebe o valor da variavel mas nao mostra no terminal
+ #print("a soma e ingual {}")
